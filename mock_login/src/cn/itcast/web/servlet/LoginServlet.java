d29580612bd014f29a75b8b2c40048850f422095
liuzhichao <3104314321@qq.com>
2021-03-14 14:53:00 +0800

    修改上一次提交 2020.03.14

--- mock_login/src/cn/itcast/web/servlet/LoginServlet.java
+++ mock_login/src/cn/itcast/web/servlet/LoginServlet.java
@@ -7,8 +7,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.beanutils.BeanUtils;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
 @WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
     @Override
@@ -28,6 +32,17 @@ public class LoginServlet extends HttpServlet {
         User loginUsers = new User();
         loginUsers.setUsername(username);
         loginUsers.setPassword(password);
+//        Map<String, String[]> parameterMap = req.getParameterMap();
+//        //3.创建User对象
+//        User loginUsers = new User();
+//        //3.1 使用beanUtils封装
+//        try {
+//            BeanUtils.populate(loginUsers,parameterMap);
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        } catch (InvocationTargetException e) {
+//            e.printStackTrace();
+//        }
         //4.调用UserDAO的login方法
         UserDao dao = new UserDao();
         //5.判断
--- mock_login/src/cn/itcast/web/servlet/SuccessServlet.java
+++ mock_login/src/cn/itcast/web/servlet/SuccessServlet.java
@@ -19,11 +19,11 @@ public class SuccessServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //1.获取共享域内容
-        User user = (User)( req.getAttribute("user"));
+        String user = req.getAttribute("user").toString();
         //2.判断
         if (user != null){
             req.setCharacterEncoding("utf-8");
-            resp.getWriter().write("登录成功," + user.getUsername() + "欢迎您");
+            resp.getWriter().write("登录成功," + user + "欢迎您");
         }
     }
 }
